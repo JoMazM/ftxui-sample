ee99809c84bc0306d7fbb20b17bc7174f79a77fd
Antonio Maza <maza.antonio@gmail.com>
2025-07-31 20:41:21 +0200

    Forward linkage option to ftxui library dependency

--- build.zig
+++ build.zig
@@ -3,10 +3,20 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     // make a list of targets that have include files and c source files
-    var targets = std.ArrayList(*std.Build.Step.Compile).init(b.allocator);
+    // var targets = std.ArrayList(*std.Build.Step.Compile).init(b.allocator);
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    
+    // External option
+    // External option
+    const linkage = b.option(std.builtin.LinkMode, "linkage", "Link mode for FTXUI libraries") orelse .static;
 
+    if (linkage == .dynamic) {
+        std.log.debug("dynamic", .{});
+    } else {
+        std.log.debug("static", .{});
+    }
+    }
     const root_module_opts: std.Build.Module.CreateOptions = .{
         .root_source_file = null,
         .target = target,
@@ -18,24 +28,21 @@ pub fn build(b: *std.Build) void {
     const ftxui_dep = b.dependency("ftxui", .{
         .target = target,
         .optimize = optimize,
+        .linkage = linkage,
     });
 
-    const exe = b.addExecutable(.{
-        .name = "ftxui_sample",
-        .root_module = b.createModule(root_module_opts),
-    });
+    const exe = b.addExecutable(.{ .name = "ftxui-sample", .root_module = b.createModule(root_module_opts), .linkage = linkage });
 
     // keep track of it, so later we can pass it to compile_commands
     // Any linked library will also be pulled in. TODO check this
-    targets.append(exe) catch @panic("OOM");
+    // targets.append(exe) catch @panic("OOM");
     // add a step called "cdb" (Compile commands DataBase) for making
     // compile_commands.json. could be named anything. cdb is just quick to type
     // zcc.createStep(b, "cdb", targets.toOwnedSlice() catch @panic("OOM"));
 
-    exe.linkLibrary(ftxui_dep.artifact("dom"));
+    // exe.linkLibrary(ftxui_dep.artifact("dom"));
     exe.linkLibrary(ftxui_dep.artifact("screen"));
-    exe.linkLibrary(ftxui_dep.artifact("component"));
-    exe.linkLibCpp();
+    // exe.linkLibrary(ftxui_dep.artifact("component"));
     exe.addIncludePath(b.path("src"));
     // exe.addWin32ResourceFile(.{ .file = .{}, .flags = &.{} });
 
@@ -48,4 +55,5 @@ pub fn build(b: *std.Build) void {
     } });
 
     b.installArtifact(exe);
+    // b.installArtifact(b.dependency("ftxui", .{}).artifact("screen"));
 }
